a1a8135ad3bb9262c71711bb67038b146a5d5414
Caleb Kathman <cjkathman@gmail.com>
2021-04-27 17:02:12 -0400

    Typo in saving system

--- includes/save.inc.php
+++ includes/save.inc.php
@@ -13,7 +13,7 @@ if(isset($_POST["user"])) {
     $serverName = "pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $dBUsername = "bzfvgoo3wymopdpx";
     $dBPassword = "x3hdio87e7mjc8o5";
-    $dBName = "	aeitaq9wuhrzbkwn";
+    $dBName = "aeitaq9wuhrzbkwn";
 
     $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
