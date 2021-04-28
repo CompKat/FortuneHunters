d251a9f6b3446a9e88988037019c011fbfa8bc9d
Caleb Kathman <cjkathman@gmail.com>
2021-04-28 16:10:31 -0400

    Removing AdSense so there is no need for privacy policy. May add back in future.

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-        <script data-ad-client="ca-pub-4433992129001330" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
+       
         <title>Fortune Hunters</title>
 
     </head>
